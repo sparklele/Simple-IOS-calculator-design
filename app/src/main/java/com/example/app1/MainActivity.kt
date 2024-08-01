4f707a854aecb45afe91fb237b7ae75875ba204f
sparklele <yereikavokock@gmail.com>
2024-08-01 15:25:01 +0300

    1

---
+++ app/src/main/java/com/example/app1/MainActivity.kt
@@ -0,0 +1,29 @@
+
+package com.example.app1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app1.databinding.ActivityMainBinding
+
+public class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    
+    private val binding: ActivityMainBinding
+      get() = checkNotNull(_binding) { "Activity has been destroyed" }
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Inflate and get instance of binding
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // set content view to binding's root
+        setContentView(binding.root)
+    }
+    
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
